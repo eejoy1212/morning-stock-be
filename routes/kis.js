908261efe98595d7b536ade4bb87bf5167661c32
원희 <eejoy1212@gmail.com>
2025-07-11 04:54:00 +0900

    xlsx-style 임시 주석

--- routes/kis.js
+++ routes/kis.js
@@ -1,6 +1,6 @@
 // //로컬용
-// import dotenv from 'dotenv';
-// dotenv.config();
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import axios from 'axios';
 import dayjs from 'dayjs';
